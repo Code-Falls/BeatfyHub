1c8c90d9c58a80d2809f628cc814ced807eee28a
gabriel_cisneiros_ <gabrielcisneirossoliveira@gmail.com>
2023-08-11 01:28:50 -0300

    all downbar buttons animated

--- src/main/java/com/example/beatfyhub/MainController.java
+++ src/main/java/com/example/beatfyhub/MainController.java
@@ -15,6 +15,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.FileChooser;
@@ -33,22 +35,26 @@ public class MainController {
     private GridPane musicGridPane;
 
     @FXML
+    private HBox homeContainer, exploreContainer, recentPlayedContainer;
+
+    @FXML
     private Button moreButton, homeButton, exploreButton, recentButton, ftbButton, likedButton, artistsButton, albumsButton, loginButton;
 
     @FXML
     private ImageView playButton, previousButton, nextButton, loopButton, shuffleButton, favoriteButton;
+
     @FXML
-    private void moreButtonClick(ActionEvent event){
+    private void moreButtonClick(ActionEvent event) {
         System.out.println("botão more clicado");
     }
 
     @FXML
-    private void homeButtonClick(ActionEvent event){
+    private void homeButtonClick(ActionEvent event) {
         System.out.println("botão home clicado");
     }
 
     @FXML
-    private void exploreButtonClick(ActionEvent event){
+    private void exploreButtonClick(ActionEvent event) {
         System.out.println("botão explore clicado");
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Arquivos MP3", "*.mp3"));
@@ -75,52 +81,52 @@ public class MainController {
     }
 
     @FXML
-    private void recentButtonClick(ActionEvent event){
+    private void recentButtonClick(ActionEvent event) {
         System.out.println("botão recent played clicado");
     }
 
     @FXML
-    private void ftbButtonClick(ActionEvent event){
+    private void ftbButtonClick(ActionEvent event) {
         System.out.println("botão feel the beat clicado");
     }
 
     @FXML
-    private void likedButtonClick(ActionEvent event){
+    private void likedButtonClick(ActionEvent event) {
         System.out.println("botão liked clicado");
     }
 
     @FXML
-    private void artistsButtonClick(ActionEvent event){
+    private void artistsButtonClick(ActionEvent event) {
         System.out.println("botão artists clicado");
     }
 
     @FXML
-    private void albumsButtonClick(ActionEvent event){
+    private void albumsButtonClick(ActionEvent event) {
         System.out.println("botão albums clicado");
     }
 
     @FXML
-    private void loginButtonClick(ActionEvent event){
+    private void loginButtonClick(ActionEvent event) {
         System.out.println("botão login spotify clicado");
     }
 
     @FXML
-    private void favoriteMedia(){
+    private void favoriteMedia() {
         System.out.println("media favoritada");
     }
 
     @FXML
-    private void shuffleMedia(){
+    private void shuffleMedia() {
         System.out.println("modo aleatorio");
     }
 
     @FXML
-    private void previousMedia(){
+    private void previousMedia() {
         System.out.println("media anterior");
     }
 
     @FXML
-    private void playMedia(){
+    private void playMedia() {
         System.out.println("media tocando");
         if (selectedAudioFile != null) {
             if (!isPlaying) {
@@ -148,56 +154,6 @@ public class MainController {
         }
     }
 
-    private void applyColorAnimation(ImageView imageView) {
-        imageView.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("Entrou");
-                imageView.setOpacity(0.5);
-                imageView.setStyle("-fx-background-color: #fff");
-            }
-        });
-
-        imageView.setOnMouseExited(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("Saiu");
-                imageView.setOpacity(1.0);
-                imageView.setStyle("-fx-background-color: initial");
-            }
-        });
-    }
-
-    @FXML
-    private void toColorPlayButton(){
-        applyColorAnimation(playButton);
-    }
-    @FXML
-    private void toColorFavoriteButton() {
-        applyColorAnimation(favoriteButton);
-    }
-
-    @FXML
-    private void toColorPreviousButton() {
-        applyColorAnimation(previousButton);
-    }
-
-    @FXML
-    private void toColorNextButton() {
-        applyColorAnimation(nextButton);
-    }
-
-    @FXML
-    private void toColorShuffleButton() {
-        applyColorAnimation(shuffleButton);
-    }
-
-    @FXML
-    private void toColorLoopButton() {
-        applyColorAnimation(loopButton);
-    }
-
-
     @FXML
     private void nextMedia(){
         System.out.println("proxima media");
@@ -206,5 +162,87 @@ public class MainController {
     @FXML
     private void loopMedia(){
         System.out.println("modo loop");
+    }
+
+
+    private void applyColorAnimationDownBar(ImageView imageView) {
+        imageView.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent1) {
+                System.out.println("Entrou");
+                imageView.setOpacity(0.5);
+            }
+        });
+
+        imageView.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Saiu");
+                imageView.setOpacity(1.0);
+            }
+        });
+    }
+
+    @FXML
+    private void toColorPlayButton() {
+        applyColorAnimationDownBar(playButton);
+    }
+
+    @FXML
+    private void toColorFavoriteButton() {
+        applyColorAnimationDownBar(favoriteButton);
+    }
+
+    @FXML
+    private void toColorPreviousButton() {
+        applyColorAnimationDownBar(previousButton);
+    }
+
+    @FXML
+    private void toColorNextButton() {
+        applyColorAnimationDownBar(nextButton);
+    }
+
+    @FXML
+    private void toColorShuffleButton() {
+        applyColorAnimationDownBar(shuffleButton);
+    }
+
+    @FXML
+    private void toColorLoopButton() {
+        applyColorAnimationDownBar(loopButton);
+    }
+
+    private void applyColorAnimationSideBar(HBox hBox) {
+        hBox.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent2) {
+                System.out.println("Entrou");
+                hBox.setStyle("-fx-background-color: #060606");
+            }
+        });
+
+        hBox.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.out.println("Saiu");
+                hBox.setStyle("-fx-background-color: #121212");
+            }
+        });
+    }
+
+    @FXML
+    private void toColorHomeButton() {
+        applyColorAnimationSideBar(homeContainer);
+    }
+
+    @FXML
+    private void toColorExploreButton() {
+        applyColorAnimationSideBar(exploreContainer);
+    }
+
+    @FXML
+    private void toColorRecentPlayedButton() {
+        applyColorAnimationSideBar(recentPlayedContainer);
     }
 }
